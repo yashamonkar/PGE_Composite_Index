5dcc1e98afcacbf9fbabc661363575f649fd2b62
yashamonkar <39978782+yashamonkar@users.noreply.github.com>
2023-12-26 19:53:06 -0500

    Updated Plots

--- codebase/Composite_Index_No_Tax_v2.R
+++ codebase/Composite_Index_No_Tax_v2.R
@@ -51,8 +51,12 @@ reg_dataset <- data.frame(streamflow = log(streamflow),
                           Net_revenue = net_revenue$Net_revenue)
 reg_dataset <- reg_dataset[-which.min(reg_dataset$Net_revenue), ]
 
+
+pdf("figures/Risk_mitigation_no_tax_v2.pdf", height=3700/600, width=6500/600)
+
 #______________________________________________________________________#
 #Divide into training and testing. 
+set.seed(1)
 train_frac <- 0.9
 
 #Seperate into Testing and Training Datasets
@@ -134,7 +138,7 @@ mtext(paste0("Training Correlation (R) is ", train_cor, ". The Adjusted R-sq is 
 
 
 ###------Composite Index----------###
-reg_composite = lm(Net_revenue ~ streamflow + CDD + Natural_Gas + streamflow*CDD + streamflow*Natural_Gas + Natural_Gas*CDD,
+reg_composite = lm(Net_revenue ~ streamflow + CDD + Natural_Gas,
                    train_dataset)
 
 newdata = data.frame(streamflow = test_dataset$streamflow,
@@ -229,7 +233,7 @@ match_var <- function(upper, lower, reg, newdata, portfolio_var, unhedged){
   pred_rev <- predict(reg , newdata)
   
   #Create sequences of strikes -- Percentiles
-  strike_seq <- seq(from = lower, to = upper, length = 100) 
+  strike_seq <- seq(from = lower, to = upper, length = 400) 
   strike <- quantile(pred_rev, strike_seq)
   
   #Payout Matrix
@@ -447,3 +451,290 @@ legend("topleft", legend=c("Unmanaged", "Composite Index"),
        col=c("black", "red"), lty=1, lwd = 2, cex=0.95,
        title="Net Revenue", text.font=4, bg='lightblue')
 
+
+#______________________________________________________________________#
+###Additional Plots and Checks
+print(paste0("The Variance of Unmanaged net revenues is ", var(1000*reg_dataset$Net_revenue)))
+print(paste0("The Variance of portfolio index is ", var(1000*hedged_net_revenues_portfolio)))
+print(paste0("The Variance of composite index is ", var(1000*Composite_index$hedged_revenue)))
+
+
+print(paste0("The 5th percentile of Unmanaged net revenues is ", 1000*quantile(reg_dataset$Net_revenue, 0.05)))
+print(paste0("The 5th percentile of portfolio index is ", 1000*quantile(hedged_net_revenues_portfolio, 0.05)))
+print(paste0("The 5th percentile  of composite index is ", 1000*quantile(Composite_index$hedged_revenue, 0.05)))
+
+
+
+
+#----------------------------------------------------------------------#
+#Create the plot
+plot(reg_dataset$Net_revenue, type='l', main = "Annual Net Revenue",
+     xlab = "Years", ylab = "Net Revenue")
+lines(1:nrow(reg_dataset), hedged_net_revenues_portfolio, col='red')
+lines(1:nrow(reg_dataset), Composite_index$hedged_revenue, col='blue')
+abline(h = mean(reg_dataset$Net_revenue), lwd = 2 , lty = 2)
+abline(h = mean(hedged_net_revenues_portfolio), lwd = 2 , lty = 2, col='red')
+abline(h = mean(Composite_index$hedged_revenue), lwd = 2 , lty = 2, col='blue')
+abline(h = min(reg_dataset$Net_revenue), lwd = 2)
+abline(h = min(hedged_net_revenues_portfolio), lwd = 2 , col='red')
+abline(h = min(Composite_index$hedged_revenue), lwd = 2 , col='blue')
+abline(h = max(reg_dataset$Net_revenue), lwd = 2 , lty = 2)
+abline(h = max(hedged_net_revenues_portfolio), lwd = 2 , lty = 2, col='red')
+abline(h = max(Composite_index$hedged_revenue), lwd = 2 , lty = 2, col='blue')
+abline(h = quantile(reg_dataset$Net_revenue, ned_var), lwd = 2,  lty = 2)
+abline(h = quantile(hedged_net_revenues_portfolio, ned_var), lwd = 2 , lty = 2 , col='red')
+abline(h = quantile(Composite_index$hedged_revenue, ned_var), lwd = 2 , lty = 2 , col='blue')
+legend("topleft", legend=c("Unmanaged", "Composite Index", "Portfolio"),
+       col=c("black", "blue", "red"), lty=1, lwd = 2, cex=0.95,
+       title="Net Revenue", text.font=4, bg='lightblue')
+
+par(mfrow=c(1,1))
+
+
+
+#----------------------------------------------------------------------#
+plt_dataset <- data.frame(Revenue = c(reg_dataset$Net_revenue, 
+                                      hedged_net_revenues_portfolio),
+                          Type = c(rep("Unmanaged", nrow(reg_dataset)),
+                                   rep("Portfolio", nrow(reg_dataset))))
+
+p1 <- ggplot(plt_dataset, aes(x=Revenue, fill = Type, pattern = Type)) +
+  geom_histogram_pattern(
+    pattern_angle = 135,
+    pattern_density = 0.1,
+    pattern_spacing = 0.025,
+    color = 'black',
+    position = 'identity', 
+    alpha = 0.5, bins=30) +
+  geom_vline(xintercept = min(reg_dataset$Net_revenue),
+             color = "#E69F00", size = 1) +
+  geom_vline(xintercept = min(hedged_net_revenues_portfolio),
+             color = "#000000", size = 1) +
+  #geom_vline(xintercept = quantile(reg_dataset$Net_revenue, ned_var),
+  #           color = "#E69F00", size = 1, linetype = "dashed") +
+  #geom_vline(xintercept = quantile(hedged_net_revenues_portfolio, ned_var),
+  #           color = "#000000", size = 1, linetype = "dashed") +
+  scale_fill_manual(values = c("#FFFFFF", "#E69F00")) +
+  xlab("Revenue ($B)") +
+  ylab("Count (No. of Years)") +
+  scale_pattern_manual(values = c(Portfolio = "stripe", Unmanaged = "none")) +
+  ggtitle(paste0("Portfolio of Contracts \nCost - Annual Premium - ", 1000*round(premium_portfolio, 3), "$ Million")) +
+  xlim(c(min(reg_dataset$Net_revenue),max(reg_dataset$Net_revenue))) +
+  theme_bw() +
+  theme(legend.position = "bottom",
+        plot.title = element_text(size=20)) 
+
+
+
+
+
+
+plt_dataset <- data.frame(Revenue = c(reg_dataset$Net_revenue,
+                                      Composite_index$hedged_revenue),
+                          Type = c(rep("Unmanaged", nrow(reg_dataset)),
+                                   rep("Composite_Index", nrow(reg_dataset))))
+
+
+p2 <- ggplot(plt_dataset, aes(x=Revenue, fill = Type, pattern = Type)) +
+  geom_histogram_pattern(
+    pattern_angle = 135,
+    pattern_density = 0.1,
+    pattern_spacing = 0.025,
+    color = 'black',
+    position = 'identity', 
+    alpha = 0.5, bins=30) +
+  geom_vline(xintercept = min(reg_dataset$Net_revenue),
+             color = "#E69F00", size = 1) +
+  geom_vline(xintercept = min(Composite_index$hedged_revenue),
+             color = "#000000", size = 1) +
+  #geom_vline(xintercept = quantile(reg_dataset$Net_revenue, ned_var),
+  #           color = "#E69F00", size = 1, linetype = "dashed") +
+  #geom_vline(xintercept = quantile(composite_index$hedged_revenue, ned_var),
+  #           color = "#000000", size = 1, linetype = "dashed") +
+  scale_fill_manual(values = c("#FFFFFF", "#E69F00")) +
+  scale_pattern_manual(values = c(Composite_Index = "wave", Unmanaged = "none")) +
+  xlab("Revenue ($B)") +
+  ylab("Count (No. of Years)") +
+  ggtitle(paste0("Composite Index Contract \nCost - Annual Premium - ", 1000*round(Composite_index$premium, 3), "$ Million")) +
+  xlim(c(min(reg_dataset$Net_revenue),max(reg_dataset$Net_revenue))) +
+  theme_bw() +
+  theme(legend.position = "bottom",
+        plot.title = element_text(size=20)) 
+
+
+plot_grid(p1,p2,
+          nrow =1)
+
+
+#------------------------------------------------------------------------------------------#
+# Figure 10 --> Add the boxplots for both 
+
+#Unmanaged Losses
+Unmanaged_Losses <- quantile(reg_dataset$Net_revenue, Composite_index$strike) - reg_dataset$Net_revenue
+Unmanaged_Losses[Unmanaged_Losses < 0] = 0
+
+
+plt_dataset <- data.frame(Unmanaged_Loss = -Unmanaged_Losses*1000,
+                          Composite = Composite_index$Payout*1000,
+                          Streamflow = sf_index$Payout*1000,
+                          CDD = cdd_index$Payout*1000,
+                          Natural_Gas = NG_index$Payout*1000)
+plt_dataset <- t(plt_dataset)
+
+
+
+#ns <- sample(1:470, 1) #ns <- 108, 392 106,316 #55,321, 58, 393, 322
+#plt_dataset <- plt_dataset[,ns:(ns+20)]
+#colnames(plt_dataset) <- c(1:21)
+
+ns <- c( 222,  21, 209, 188, 401, 261,  213, 296, 109, 497, 225, 263, 155, 176,  17, 290, 285, 80, 436,   172) #sample(1:ncol(plt_dataset), 20)
+plt_dataset <- plt_dataset[,ns]
+colnames(plt_dataset) <- c(1:20)
+
+
+plt2 <- plt_dataset[2:5,]
+
+x <- 1:100
+y <- rep(plt_dataset[1,], each = 5)
+
+
+
+par(mfrow = c(1,1),  mar = c(5, 5, 3, 2))
+
+barplot(plt2,
+        col = c("#0096FF", "#DCDCDC", "#808080", "#B2BEB5"),
+        beside = TRUE,
+        ylim = c(-350, 350), border = TRUE,
+        ylab = "($ Million)", 
+        xlab = "Years", cex.lab=1.5, cex.legend = 1.75)
+lines(x,y, lwd = 3, lty = 2 , col ='red')
+for(i in 1:20){
+  lines(x= c(0.5+5*i, 0.5+5*i), y=c(0,280), lwd = 0.05, lty =2)}
+legend('topright', 
+       legend  = c("Unmanaged Loss",  "Composite Payout", "Streamflow Payout", "Temperature Payout", "Natural Gas Payout"),
+       col = c("red","#0096FF", "#DCDCDC", "#808080", "#B2BEB5"),
+       pch = 15, cex = 0.9, ncol=5, 
+       pt.cex=c(0,2.5,2.5,2.5,2.5,2.5),
+       lwd=c(2,NA,NA,NA,NA,NA),
+       lty=c(2,NA,NA,NA,NA,NA))
+
+
+
+#------------------------------------------------------------------------------------------#
+# Figure 11 --> Add the boxplots for both 
+
+plt_dataset <- data.frame(Revenue = c(Unmanaged_Losses, 
+                                      payout_portfolio),
+                          Type = c(rep("Unmanaged", nrow(reg_dataset)),
+                                   rep("Portfolio", nrow(reg_dataset))))  
+
+
+p1 <- ggplot(plt_dataset, aes(x=Revenue, fill = Type, pattern = Type)) +
+  geom_histogram_pattern(
+    pattern_angle = 135,
+    pattern_density = 0.1,
+    pattern_spacing = 0.025,
+    color = 'black',
+    position = 'identity', 
+    alpha = 0.5, bins=30) +
+  scale_fill_manual(values = c("#FFFFFF", "#E69F00")) +
+  scale_pattern_manual(values = c(Portfolio = "stripe", Unmanaged = "none")) +
+  ggtitle(paste0("Portfolio of Contracts - Payouts \nAnnual Premium - ", 1000*round(premium_portfolio, 3), "$ Million")) +
+  xlim(c(-0.1,0.35)) +
+  ylim(c(0,500)) +
+  xlab("Revenue ($B)") +
+  ylab("Count (No. of Years)") +
+  theme_bw() +
+  theme(legend.position = "bottom",
+        plot.title = element_text(size=20)) 
+
+
+plt_dataset <- data.frame(Revenue = c(Unmanaged_Losses, 
+                                      Composite_index$Payout),
+                          Type = c(rep("Unmanaged", nrow(reg_dataset)),
+                                   rep("Composite_Index", nrow(reg_dataset))))  
+
+
+p2 <- ggplot(plt_dataset, aes(x=Revenue, fill = Type, pattern = Type)) +
+  geom_histogram_pattern(
+    pattern_angle = 135,
+    pattern_density = 0.1,
+    pattern_spacing = 0.025,
+    color = 'black',
+    position = 'identity', 
+    alpha = 0.5, bins=30) +
+  scale_fill_manual(values = c("#FFFFFF", "#E69F00")) +
+  scale_pattern_manual(values = c(Composite_Index = "wave", Unmanaged = "none")) +
+  ggtitle(paste0("Composite Index Contract - Payouts \nAnnual Premium - ", 1000*round(Composite_index$premium, 3), "$ Million")) +
+  xlim(c(-0.05,0.35)) +
+  ylim(c(0,500)) +
+  xlab("Revenue ($B)") +
+  ylab("Count (No. of Years)") +
+  theme_bw() +
+  theme(legend.position = "bottom",
+        plot.title = element_text(size=20)) 
+
+plot_grid(p1,p2,
+          nrow =1)
+
+
+
+#------------------------------------------------------------------------------------------#
+# Figure 11 --> Add the boxplots for both 
+
+plt_dataset <- data.frame(Revenue = c(Unmanaged_Losses, 
+                                      payout_portfolio),
+                          Type = c(rep("Unmanaged", nrow(reg_dataset)),
+                                   rep("Portfolio", nrow(reg_dataset))))  
+
+
+
+p1 <- ggplot(plt_dataset, aes(x=Revenue, fill = Type, pattern = Type)) +
+  geom_histogram_pattern(
+    pattern_angle = 135,
+    pattern_density = 0.1,
+    pattern_spacing = 0.025,
+    color = 'black',
+    position = 'identity', 
+    alpha = 0.5, bins=30) +
+  scale_fill_manual(values = c("#FFFFFF", "#E69F00")) +
+  scale_pattern_manual(values = c(Portfolio = "stripe", Unmanaged = "none")) +
+  ggtitle(paste0("Portfolio of Contracts - Payouts \nCost - Annual Premium - ", 1000*round(premium_portfolio, 3), "$ Million")) +
+  xlim(c(-0.1,0.35)) +
+  ylim(c(0,100)) +
+  xlab("Revenue ($B)") +
+  ylab("Count (No. of Years)") +
+  theme_bw() +
+  theme(legend.position = "bottom",
+        plot.title = element_text(size=20)) 
+
+
+plt_dataset <- data.frame(Revenue = c(Unmanaged_Losses, 
+                                      Composite_index$payout),
+                          Type = c(rep("Unmanaged", nrow(reg_dataset)),
+                                   rep("Composite_Index", nrow(reg_dataset))))  
+
+
+p2 <- ggplot(plt_dataset, aes(x=Revenue, fill = Type, pattern = Type)) +
+  geom_histogram_pattern(
+    pattern_angle = 135,
+    pattern_density = 0.1,
+    pattern_spacing = 0.025,
+    color = 'black',
+    position = 'identity', 
+    alpha = 0.5, bins=30) +
+  scale_fill_manual(values = c("#FFFFFF", "#E69F00")) +
+  scale_pattern_manual(values = c(Composite_Index = "wave", Unmanaged = "none")) +
+  ggtitle(paste0("Composite Index Contract - Payouts \nCost - Annual Premium - ", 1000*round(Composite_index$premium, 3), "$ Million")) +
+  xlim(c(-0.05,0.35)) +
+  ylim(c(0,100)) +
+  xlab("Revenue ($B)") +
+  ylab("Count (No. of Years)") +
+  theme_bw() +
+  theme(legend.position = "bottom",
+        plot.title = element_text(size=20)) 
+
+plot_grid(p1,p2,
+          nrow =1)
+
+dev.off()
